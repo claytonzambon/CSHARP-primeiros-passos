effb45e6d7d8d51220c4eabfbc4ed27999b46f25
Clayton Zambon <zambon@outlook.com.br>
2020-11-18 12:04:49 -0300

    Second commit

---
+++ 03-Estrutura_Sequencial/12_Primeiro_Programa/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _12_Primeiro_Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá Mundo !!!");
+            Console.WriteLine("\n\n");
+        }
+    }
+}
