22d26e3a30c403551d40a3e2a8498620f2e9add4
Clayton Zambon <zambon@outlook.com.br>
2020-11-19 12:41:38 -0300

    16_Processamento_de_Dados_Exemplo_04

---
+++ 03-Estrutura_Sequencial/16_Processamento_de_Dados_Exemplo_04/16_Processamento_de_Dados_Exemplo_04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _16_Processamento_de_Dados_Exemplo_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Casting
+            //É a conversão explícita de um tipo para outro
+            //É necéssário quando o compilador não é capax de adivinhar que o resultado
+            //de uma expressão deve ser de outro tipo
+            int a, b;
+            double resultado;
+
+            a = 5;
+            b = 2;
+
+            resultado = (double)a/b;
+
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+        }
+    }
+}
