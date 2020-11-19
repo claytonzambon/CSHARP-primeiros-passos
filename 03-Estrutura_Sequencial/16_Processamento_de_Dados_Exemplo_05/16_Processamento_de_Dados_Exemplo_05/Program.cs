6c7ed5f5bf204aaad4b6d62302b8f364bbbfc3ac
Clayton Zambon <zambon@outlook.com.br>
2020-11-19 12:44:40 -0300

    16_Processamento_de_Dados_Exemplo_05

---
+++ 03-Estrutura_Sequencial/16_Processamento_de_Dados_Exemplo_05/16_Processamento_de_Dados_Exemplo_05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _16_Processamento_de_Dados_Exemplo_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            int b;
+
+            a = 5.0;
+            b = (int)a; //Casting
+
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
