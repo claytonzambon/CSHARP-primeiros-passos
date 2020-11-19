e42bfece1a1c05c22a7364dad87df64c48d88764
Clayton Zambon <zambon@outlook.com.br>
2020-11-19 12:31:20 -0300

    16_Processamento_de_Dados_Exemplo_02

---
+++ 03-Estrutura_Sequencial/16_Processamento_de_Dados_Exemplo_02/16_Processamento_de_Dados_Exemplo_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _16_Processamento_de_Dados_Exemplo_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            double y;
+
+            x = 5;
+            y = 2 * x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.ReadLine();
+        }
+    }
+}
