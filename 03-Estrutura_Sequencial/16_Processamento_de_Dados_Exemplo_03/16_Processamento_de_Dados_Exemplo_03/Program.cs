9e79feeb8febd40c086da39624433b79c64caa9e
Clayton Zambon <zambon@outlook.com.br>
2020-11-19 12:39:08 -0300

    16_Processamento_de_Dados_Exemplo_03

---
+++ 03-Estrutura_Sequencial/16_Processamento_de_Dados_Exemplo_03/16_Processamento_de_Dados_Exemplo_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _16_Processamento_de_Dados_Exemplo_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculando a área do Trapézio
+            //Dados: b = 6, h = 5, B = 8
+            //       b = 6
+            //      ------------
+            //      |           |
+            //      |            |
+            //  h=5 |             |
+            //      |              |
+            //      ----------------
+            //          B = 8
+            //Fórmula: area = ((b + B)/2) * h
+
+            double b, B, h, area;
+
+            b = 6.0;
+            B = 8.0;
+            h = 5.0;
+
+            area = (b + B) / 2.0 * h;
+
+            Console.WriteLine(area);
+            Console.ReadLine();
+        }
+    }
+}
