89f3e85020752005a46389d337dd8a5da5e3a1ba
Clayton Zambon <zambon@outlook.com.br>
2020-11-19 14:20:34 -0300

    20.1_Exercicio_01

---
+++ 03-Estrutura_Sequencial/20.1_Exercicio_01/20.1_Exercicio_01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace _20._1_Exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Estrutura Sequencial - Exercício 01
+            *  Fazer um programa para ler as medidas da largura e comprimento de um 
+            *  terreno retangular com uma casa decimal, bem como o valor do metro quadrado
+            *  do terreno com duas casas decimais. Em seguida, o programa deve mostrar
+            *  o valor da área do terreno, bem como o valor do preço do terreno,
+            *  ambos com duas casas decimais, conforme exemplo
+
+            * - Exemplo:
+            * | Entrada:  | Saída:			    |
+            * |-----------|---------------------|
+            * | 10.0      | AREA = 300.00		|
+            * | 30.0	  | PRECO = 60000.00    |
+            * | 200.00	  |					    |
+            */
+
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = (largura * comprimento);
+            preco = (precoMetroQuadrado * area);
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
