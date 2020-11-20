d1212272d3f6bfcc6f3cd95c8561a588ddb91b65
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 15:45:55 -0300

    26.1_Exercicio_URI_1018

---
+++ 03-Estrutura_Sequencial/27.1_Exercicio_URI_1019/27.1_Exercicio_URI_1019/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _27._1_Exercicio_URI_1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1019
+             */
+
+            int N, horas, resto, minutos, segundos;
+
+            N = int.Parse(Console.ReadLine());
+            horas = N / 3600;
+            resto = N % 3600;
+
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
