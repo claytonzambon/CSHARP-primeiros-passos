6dd79049007635e7dd415b3fd747e4509532dee7
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 10:44:41 -0300

    23.2_Exercicio_URI_1003

---
+++ 03-Estrutura_Sequencial/23.2_Exercicio_URI_1003/23.2_Exercicio_URI_1003/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _23._2_Exercicio_URI_1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1003
+             */
+
+            int A, B, SOMA;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            SOMA = A + B;
+                       
+            Console.WriteLine("SOMA = " + SOMA);
+        }
+    }
+}
