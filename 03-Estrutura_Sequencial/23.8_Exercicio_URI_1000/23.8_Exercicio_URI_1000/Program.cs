58ad9aa9ad6e00c718662825135c49f89d3acadf
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 11:44:03 -0300

    23.9_Exercicio_URI_1009

---
+++ 03-Estrutura_Sequencial/23.8_Exercicio_URI_1000/23.8_Exercicio_URI_1000/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _23._8_Exercicio_URI_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 03-Estrutura_Sequencial/23.9_Exercicio_URI_1009/23.9_Exercicio_URI_1009/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace _23._9_Exercicio_URI_1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1009
+             */
+
+            string nome;
+            double salarioFixo, totalVendas, totalReceber;
+
+            nome = Console.ReadLine();
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            totalReceber = salarioFixo + (totalVendas * 0.15);
+
+            Console.WriteLine("TOTAL = R$ " + totalReceber.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
