867307da90932af5343948e4f9ebb2c97626fc26
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 12:56:05 -0300

    23.16_Exercicio_URI_1016

---
+++ 03-Estrutura_Sequencial/23.16_Exercicio_URI_1016/23.16_Exercicio_URI_1016/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _23._16_Exercicio_URI_1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1016
+             */
+
+            int distancia, tempo;
+
+            distancia = int.Parse(Console.ReadLine());
+            tempo = distancia * 2;
+
+            Console.WriteLine(tempo + " minutos");
+        }
+    }
+}
