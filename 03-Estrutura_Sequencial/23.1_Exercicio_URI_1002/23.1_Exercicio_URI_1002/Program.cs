38c20b32af8e561b7015df13ce0a97a8df4fbbc7
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 10:40:50 -0300

    23.1_Exercicio_URI_1002

---
+++ 03-Estrutura_Sequencial/23.1_Exercicio_URI_1002/23.1_Exercicio_URI_1002/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace _23._1_Exercicio_URI_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1002
+            */
+
+            double area, n, raio;
+
+            n = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = n * Math.Pow(raio, 2.0);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
