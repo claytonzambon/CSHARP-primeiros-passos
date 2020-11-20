e326b3416652e945c4de0ddb65595b2738c587a1
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 15:36:51 -0300

    26.1_Exercicio_URI_1018

---
+++ 03-Estrutura_Sequencial/26.1_Exercicio_URI_1018/26.1_Exercicio_URI_1018/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _26._1_Exercicio_URI_1018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1018
+             */
+
+            int N, quociente, resto, nota;
+
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine(N);
+
+            resto = N;
+
+            nota = 100;
+            quociente = resto / 100;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 50;
+            quociente = resto / 50;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 20;
+            quociente = resto / 20;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 10;
+            quociente = resto / 10;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 5;
+            quociente = resto / 5;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 2;
+            quociente = resto / 2;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            Console.WriteLine(resto + " nota(s) de R$ 1,00");
+        }
+    }
+}
