aef120104eb69b5c7d42b0329d4200ec73007ebd
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 12:23:34 -0300

    23.12_Exercicio_URI_1012

---
+++ 03-Estrutura_Sequencial/23.12_Exercicio_URI_1012/23.12_Exercicio_URI_1012/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace _23._12_Exercicio_URI_1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1012
+             */
+
+            double A, B, C, pi, triangulo, circulo, trapezio, quadrado, retangulo;
+
+            string[] vetor = Console.ReadLine().Split(' ');
+            A = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+            B = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+            C = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+            pi = 3.14159;
+
+            triangulo = (A * C) / 2;
+            circulo = pi * Math.Pow(C, 2.0);
+            trapezio = ((A + B) * C) / 2;
+            quadrado = B * B;
+            retangulo = A * B;
+        
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
