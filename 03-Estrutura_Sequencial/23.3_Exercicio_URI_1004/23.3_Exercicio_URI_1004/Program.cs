0a6c01cb77f7119058c79a97dcb06b722e370704
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 10:48:59 -0300

    23.3_Exercicio_URI_1004

---
+++ 03-Estrutura_Sequencial/23.3_Exercicio_URI_1004/23.3_Exercicio_URI_1004/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _23._3_Exercicio_URI_1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1004
+             */
+
+            int A, B, PROD;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            PROD = A * B;
+            
+            Console.WriteLine("PROD = " + PROD);
+        }
+    }
+}
