08046e99404cd672f33c07ed2e4f749329c6bc85
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 11:09:00 -0300

    23.6_Exercicio_URI_1007

---
+++ 03-Estrutura_Sequencial/23.6_Exercicio_URI_1007/23.6_Exercicio_URI_1007/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _23._6_Exercicio_URI_1007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1007
+             */
+
+            int A, B, C, D, DIFERENCA;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            DIFERENCA = (A * B - C * D);
+
+            Console.WriteLine("DIFERENCA = " + DIFERENCA);
+        }
+    }
+}
