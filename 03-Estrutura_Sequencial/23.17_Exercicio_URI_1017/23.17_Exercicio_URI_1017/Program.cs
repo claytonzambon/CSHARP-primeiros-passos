cb294427287fb1fa26959bf67f7d80d8ac9d2358
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 13:05:21 -0300

    23.17_Exercicio_URI_1017

---
+++ 03-Estrutura_Sequencial/23.17_Exercicio_URI_1017/23.17_Exercicio_URI_1017/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _23._17_Exercicio_URI_1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1017
+             */
+
+            int tempoGasto, velocidadeMedia;
+            double combustivelGasto;
+
+            tempoGasto = int.Parse(Console.ReadLine());
+            velocidadeMedia = int.Parse(Console.ReadLine());
+            combustivelGasto = (double)(tempoGasto * velocidadeMedia) / 12;
+
+            Console.WriteLine(combustivelGasto.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
