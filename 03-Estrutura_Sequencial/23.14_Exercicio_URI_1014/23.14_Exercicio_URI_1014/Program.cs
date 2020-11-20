003e2fb32a175bc1eebbb1a87f641b3489802da2
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 12:39:11 -0300

    23.14_Exercicio_URI_1014

---
+++ 03-Estrutura_Sequencial/23.14_Exercicio_URI_1014/23.14_Exercicio_URI_1014/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _23._14_Exercicio_URI_1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1014
+             */
+
+            int X;
+            double Y, consumoMedio;
+
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            consumoMedio = X / Y;
+
+            Console.WriteLine(consumoMedio.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
