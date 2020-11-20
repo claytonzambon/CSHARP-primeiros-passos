9cfc52dbfcf1ca996582f2df48d32562ad7e5688
Clayton Zambon <zambon@outlook.com.br>
2020-11-20 10:59:08 -0300

    23.4_Exercicio_URI_1005

---
+++ 03-Estrutura_Sequencial/23.4_Exercicio_URI_1005/23.4_Exercicio_URI_1005/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _23._4_Exercicio_URI_1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1005
+             */
+
+            double A, B, MEDIA;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
