eba3c47ead78f6627bf5aa4399ed51cd233a08af
Clayton Zambon <zambon@outlook.com.br>
2020-11-23 14:00:13 -0300

    28.3_Exercicio_URI_1061

--- 03-Estrutura_Sequencial/28.2_Exercicio_URI_1021/28.2_Exercicio_URI_1021/Program.cs
+++ 03-Estrutura_Sequencial/28.2_Exercicio_URI_1021/28.2_Exercicio_URI_1021/Program.cs
@@ -16,9 +16,21 @@ namespace _28._2_Exercicio_URI_1021
 
             N = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            // Vamos multiplicar N por 100 e forçar a conversão para int.
+            // Desse modo, por exemplo, 576.73 vai se tornar 57673
+
+            // Vamos tambem somar 0.5 antes de converter, para assegurar 
+            // que o numero seja devidamente arredondado, pois o tipo double
+            // as vezes da problema de arredondamento (por exemplo: se digitarmos
+            // 576.81 e multiplicarmos por 100, o resultado sera 57680.99999999,
+            // dai o casting resultaria em 57680 e nao 57681 como desejado)
+
             resto = (int)(N * 100.0 + 0.5);
 
             Console.WriteLine("NOTAS:");
+
+            // como multiplicamos o valor por 100 acima, o valor de cada nota
+            // também deverá ser multiplicado por 100 a seguir
 
             nota = 100;
             quociente = resto / (nota * 100);
