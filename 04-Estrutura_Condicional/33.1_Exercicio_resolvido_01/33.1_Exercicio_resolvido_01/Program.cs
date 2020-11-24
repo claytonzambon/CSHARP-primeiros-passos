22948b54eff9e827c87c4bd1f87ec7228e391d82
Clayton Zambon <zambon@outlook.com.br>
2020-11-24 12:54:44 -0300

    Estrutura condicional e 33.1_Exercicio_resolvido_01

---
+++ 04-Estrutura_Condicional/33.1_Exercicio_resolvido_01/33.1_Exercicio_resolvido_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace _33._1_Exercicio_resolvido_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, soma;
+
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = nota1 + nota2;
+
+            Console.WriteLine("NOTA FINAL = " + soma.ToString("F1", CultureInfo.InvariantCulture));
+            if (soma < 60.00){
+                Console.WriteLine("REPROVADO");
+            }
+        }
+    }
+}
