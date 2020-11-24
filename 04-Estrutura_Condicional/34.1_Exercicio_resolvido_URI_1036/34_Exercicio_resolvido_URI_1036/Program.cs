af6000fd3b1bf48eb1e00d23865d20f68d27e306
Clayton Zambon <zambon@outlook.com.br>
2020-11-24 13:23:13 -0300

    34.1_Exercicio_resolvido_URI_1036

---
+++ 04-Estrutura_Condicional/34.1_Exercicio_resolvido_URI_1036/34_Exercicio_resolvido_URI_1036/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace _34_Exercicio_resolvido_URI_1036
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio do URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1036
+             */
+
+            double a, b, c, delta, r1, r2;
+
+            String[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            if (delta < 0.0 || a == 0) {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else {
+                r1 = (- b + Math.Sqrt(delta)) / (2.0 * a);
+                r2 = (- b - Math.Sqrt(delta)) / (2.0 * a);
+
+                Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
