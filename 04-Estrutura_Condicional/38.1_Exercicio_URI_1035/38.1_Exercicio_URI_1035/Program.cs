6fd91e8f97bc58942ab41c19fb165316d4ad4e61
Clayton Zambon <zambon@outlook.com.br>
2020-11-24 14:48:11 -0300

    38.1_Exercicio_URI_1035

---
+++ 04-Estrutura_Condicional/38.1_Exercicio_URI_1035/38.1_Exercicio_URI_1035/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _38._1_Exercicio_URI_1035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercício URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1035
+             */
+
+            int a, b, c, d;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+            d = int.Parse(vet[3]);
+
+            if (b > c && d > a && (c + d) > (a + b) && c > 0 && d > 0 && (a % 2) == 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+        }
+    }
+}
