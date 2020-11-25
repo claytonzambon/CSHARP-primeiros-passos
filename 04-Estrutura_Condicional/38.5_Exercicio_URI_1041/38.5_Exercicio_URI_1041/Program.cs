50e67685e4f97f963c46a51ccfc97d22427e1d39
Clayton Zambon <zambon@outlook.com.br>
2020-11-25 15:23:06 -0300

    38.5_Exercicio_URI_1041

---
+++ 04-Estrutura_Condicional/38.5_Exercicio_URI_1041/38.5_Exercicio_URI_1041/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace _38._5_Exercicio_URI_1041
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercício URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1041
+             */
+
+            double x, y;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            x = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x == 0.0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if (y == 0.0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (x > 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0.0 && y < 0.0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else
+            {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
