b81bc51b7f36f8bba8e4226684230e8c363c49e7
Clayton Zambon <zambon@outlook.com.br>
2020-11-26 12:19:32 -0300

    38.9_Exercicio_URI_1045

---
+++ 04-Estrutura_Condicional/38.9_Exercicio_URI_1045/38.9_Exercicio_URI_1045/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Globalization;
+
+namespace _38._9_Exercicio_URI_1045
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercício URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1045
+             */
+
+            double a, b, c, maior, meio, menor;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = double.Parse(vet[0]);
+            b = double.Parse(vet[1]);
+            c = double.Parse(vet[2]);
+
+            if (a > b && a > c)
+            {
+                maior = a;
+                if (b > c)
+                {
+                    meio = b;
+                    menor = c;
+                }
+                else
+                {
+                    meio = c;
+                    menor = b;
+                }
+            }
+            else if (b > c)
+            {
+                maior = b;
+                if (a > c)
+                {
+                    meio = a;
+                    menor = c;
+                }
+                else
+                {
+                    meio = c;
+                    menor = a;
+                }
+            }
+            else
+            {
+                maior = c;
+                if (a > b)
+                {
+                    meio = a;
+                    menor = b;
+                }
+                else
+                {
+                    meio = b;
+                    menor = a;
+                }
+            }
+
+            if (maior >= (meio + menor))
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            }
+            else
+            {
+                if (Math.Pow(maior, 2.0) == (Math.Pow(meio, 2.0) + Math.Pow(menor, 2.0)))
+                    Console.WriteLine("TRIANGULO RETANGULO");
+
+                else if (Math.Pow(maior, 2.0) > (Math.Pow(meio, 2.0) + Math.Pow(menor, 2.0)))
+                    Console.WriteLine("TRIANGULO OBTUSANGULO");
+
+                else if (Math.Pow(maior, 2.0) < (Math.Pow(meio, 2.0) + Math.Pow(menor, 2.0)))
+                    Console.WriteLine("TRIANGULO ACUTANGULO");
+
+                if (maior == meio && meio == menor)
+                    Console.WriteLine("TRIANGULO EQUILATERO");
+
+                else if (maior == meio || maior == menor || meio == menor)
+                    Console.WriteLine("TRIANGULO ISOSCELES");
+            }
+        }
+    }
+}
