e0a31d7008db99a1b8fd9e94034148026bdfef53
Clayton Zambon <zambon@outlook.com.br>
2020-11-26 14:11:04 -0300

    38.12_Exercicio_URI_1048 - Atualizado

--- 04-Estrutura_Condicional/38.12_Exercicio_URI_1048/38.12_Exercicio_URI_1048/Program.cs
+++ 04-Estrutura_Condicional/38.12_Exercicio_URI_1048/38.12_Exercicio_URI_1048/Program.cs
@@ -11,39 +11,27 @@ namespace _38._12_Exercicio_URI_1048
              * https://www.urionlinejudge.com.br/judge/pt/problems/view/1048
              */
 
-            double salario, novoSalario, reajuste;
-            int percentual;
+            double salario, novoSalario, reajuste, percentual;
 
             salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            if (salario >= 0.00 && salario <= 400.00)
-            {
-                reajuste = salario * 0.15;
-                novoSalario = salario + reajuste;
-                percentual = 15;
-            } 
-            else if (salario >= 400.01 && salario <= 800.00){
-                reajuste = salario * 0.12;
-                novoSalario = salario + reajuste;
-                percentual = 12;
-            }
-            else if (salario >= 800.01 && salario <= 1200.00) {
-                reajuste = salario * 0.10;
-                novoSalario = salario + reajuste;
-                percentual = 10;
-            } else if (salario >= 1200.01 && salario <= 2000.00){
-                reajuste = salario * 0.07;
-                novoSalario = salario + reajuste;
-                percentual = 7;
-            }
-            else {
-                reajuste = salario * 0.04;
-                novoSalario = salario + reajuste;
-                percentual = 4;
-            }
+            if (salario <= 400.00)
+                percentual = 15.0;
+            else if (salario <= 800.00)
+                percentual = 12.0;
+            else if (salario <= 1200.00) 
+                percentual = 10.0;
+            else if (salario <= 2000.00)
+                percentual = 7.0;
+            else 
+                percentual = 4.0;
+
+            reajuste = salario * percentual / 100.00;
+            novoSalario = salario + reajuste;
+
             Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Em percentual: " + percentual + " %");
+            Console.WriteLine("Em percentual: " + percentual.ToString("F0", CultureInfo.InvariantCulture) + " %");
         }
     }
 }
