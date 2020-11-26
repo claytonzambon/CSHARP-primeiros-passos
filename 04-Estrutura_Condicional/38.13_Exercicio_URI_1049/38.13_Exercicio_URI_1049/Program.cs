da39b22a4ee8fdc17f0ab252c80485c17b33c58e
Clayton Zambon <zambon@outlook.com.br>
2020-11-26 15:19:53 -0300

    38.13_Exercicio_URI_1049

---
+++ 04-Estrutura_Condicional/38.13_Exercicio_URI_1049/38.13_Exercicio_URI_1049/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _38._13_Exercicio_URI_1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercício URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1049
+             */
+
+            string animal = Console.ReadLine();
+
+            if (animal == "vertebrado")
+            {
+                animal = Console.ReadLine();
+                if (animal == "ave")
+                {
+                    animal = Console.ReadLine();
+                    if (animal == "carnivoro")
+                        Console.WriteLine("aguia");
+                    else
+                        Console.WriteLine("pomba");
+                }
+                else
+                {
+                    animal = Console.ReadLine();
+                    if (animal == "onivoro")
+                        Console.WriteLine("homem");
+                    else
+                        Console.WriteLine("vaca");
+                }
+            }
+            else
+            {
+                animal = Console.ReadLine();
+                if (animal == "inseto")
+                {
+                    animal = Console.ReadLine();
+                    if (animal == "hematofago")
+                        Console.WriteLine("pulga");
+                    else
+                        Console.WriteLine("lagarta");
+                }
+                else
+                {
+                    animal = Console.ReadLine();
+                    if (animal == "hematofago")
+                        Console.WriteLine("sanguessuga");
+                    else
+                        Console.WriteLine("minhoca");
+                }
+            }
+        }
+    }
+}
