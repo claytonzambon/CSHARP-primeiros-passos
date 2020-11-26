464666b9ff3541e87fbfce2fe1f8f95efebe5239
Clayton Zambon <zambon@outlook.com.br>
2020-11-26 13:22:08 -0300

    38.11_Exercicio_URI_1047

---
+++ 04-Estrutura_Condicional/38.11_Exercicio_URI_1047/38.11_Exercicio_URI_1047/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _38._11_Exercicio_URI_1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercício URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1047
+             */
+
+            int horaInicial, minutoInicial, horaFinal, minutoFinal, duracaoHora, duracaoMinutos, duracaoTotal;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse(vet[0]);
+            minutoInicial = int.Parse(vet[1]);
+            horaFinal = int.Parse(vet[2]);
+            minutoFinal = int.Parse(vet[3]);
+
+            int momentoInicial = horaInicial * 60 + minutoInicial;
+            int momentoFinal = horaFinal * 60 + minutoFinal;
+
+            if (momentoInicial < momentoFinal)
+                duracaoTotal = momentoFinal - momentoInicial;
+            else
+                duracaoTotal = (24 * 60 - momentoInicial) + momentoFinal;
+
+            duracaoHora = duracaoTotal / 60;
+            duracaoMinutos = duracaoTotal % 60;
+
+            Console.WriteLine("O JOGO DUROU " + duracaoHora + " HORA(S) E " + duracaoMinutos + " MINUTO(S)");
+        }
+    }
+}
