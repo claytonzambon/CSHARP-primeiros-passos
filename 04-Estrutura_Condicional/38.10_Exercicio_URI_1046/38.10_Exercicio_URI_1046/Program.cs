913af06461c8431809b292300493ed82eb51141e
Clayton Zambon <zambon@outlook.com.br>
2020-11-26 12:33:19 -0300

    38.10_Exercicio_URI_1046

---
+++ 04-Estrutura_Condicional/38.10_Exercicio_URI_1046/38.10_Exercicio_URI_1046/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _38._10_Exercicio_URI_1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercício URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1046
+             */
+
+            int horaInicial, horaFinal, duracao;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse(vet[0]);
+            horaFinal = int.Parse(vet[1]);
+
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
