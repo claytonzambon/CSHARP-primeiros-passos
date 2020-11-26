fef76adfd2870f28a1cf43dddc78806a80071bfd
Clayton Zambon <zambon@outlook.com.br>
2020-11-26 11:38:38 -0300

    38.8_Exercicio_URI_1044

---
+++ 04-Estrutura_Condicional/38.8_Exercicio_URI_1044/38.8_Exercicio_URI_1044/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _38._8_Exercicio_URI_1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercício URI Online Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1044
+             */
+
+            int a, b;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
