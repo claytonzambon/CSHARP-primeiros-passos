a55b7c2d5761f662a9ffd895a8391ffe2f0864d4
Clayton Zambon <zambon@outlook.com.br>
2020-12-01 11:00:45 -0300

    38.17_Exercicio_URI_1060 - Numeros Positivos

---
+++ 04-Estrutura_Condicional/38.17_Exercicio_URI_1060/38.17_Exercicio_URI_1060/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace _38._17_Exercicio_URI_1060 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercício URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1060
+             */
+
+            int count = 0;
+            double valor;
+
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (valor > 0.0)
+                count = count + 1;
+
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (valor > 0.0)
+                count++;
+
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (valor > 0.0)
+                count++;
+
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (valor > 0.0)
+                count++;
+
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (valor > 0.0)
+                count++;
+
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (valor > 0.0)
+                count++;
+
+            Console.WriteLine(count + " valores positivos");
+        }
+    }
+}
