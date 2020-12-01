9d51ffe21483af124f336616dc139f65658c14f1
Clayton Zambon <zambon@outlook.com.br>
2020-12-01 10:51:21 -0300

    38.16_Exercicio_URI_1052 - Mes

---
+++ 04-Estrutura_Condicional/38.16_Exercicio_URI_1052/38.16_Exercicio_URI_1052/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _38._16_Exercicio_URI_1052 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI Onlline Judge
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1052
+             */
+
+            int valor = int.Parse(Console.ReadLine());
+            string mes;
+
+            switch (valor) {
+                case 1:
+                    mes = "January";
+                    break;
+                case 2:
+                    mes = "February";
+                    break;
+                case 3:
+                    mes = "March";
+                    break;
+                case 4:
+                    mes = "April";
+                    break;
+                case 5:
+                    mes = "May";
+                    break;
+                case 6:
+                    mes = "June";
+                    break;
+                case 7:
+                    mes = "July";
+                    break;
+                case 8:
+                    mes = "August";
+                    break;
+                case 9:
+                    mes = "September";
+                    break;
+                case 10:
+                    mes = "October";
+                    break;
+                case 11:
+                    mes = "November";
+                    break;
+                default:
+                    mes = "December";
+                    break;
+            }
+
+            Console.WriteLine(mes);
+        }
+    }
+}
