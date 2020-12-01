a0d951e6ce2aa4e1d2ceb12a4e7f3584af1a8da9
Clayton Zambon <zambon@outlook.com.br>
2020-12-01 11:14:08 -0300

    38.19_Exercicio_URI_1065 - Pares entre Cinco Numeros

---
+++ 04-Estrutura_Condicional/38.19_Exercicio_URI_1065/38.19_Exercicio_URI_1065/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _38._19_Exercicio_URI_1065 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1065
+             */
+
+            int valor, count = 0;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                count = count + 1;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                count = count + 1;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                count = count + 1;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                count = count + 1;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                count = count + 1;
+
+            Console.WriteLine(count + " valores pares");
+        }
+    }
+}
