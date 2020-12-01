4bc51a584fd8942c1cdbe65e88352548f4dbab44
Clayton Zambon <zambon@outlook.com.br>
2020-12-01 16:44:51 -0300

    38.21_Exercicio_URI_1070 - Seis Numeros Impares

---
+++ 04-Estrutura_Condicional/38.21_Exercicio_URI_1070/38.21_Exercicio_URI_1070/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _38._21_Exercicio_URI_1070 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicios URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1070
+             */
+
+            int X;
+
+            X = int.Parse(Console.ReadLine());
+
+            if (X % 2 == 0)
+                X = X + 1;
+
+            Console.WriteLine(X);
+            Console.WriteLine(X = X + 2);
+            Console.WriteLine(X = X + 2);
+            Console.WriteLine(X = X + 2);
+            Console.WriteLine(X = X + 2);
+            Console.WriteLine(X = X + 2);
+        }
+    }
+}
