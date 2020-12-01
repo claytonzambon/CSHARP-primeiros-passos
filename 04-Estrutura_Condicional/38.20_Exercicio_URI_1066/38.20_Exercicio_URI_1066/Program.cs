56813710cd86acf1227638089e3ad90a6de43ea5
Clayton Zambon <zambon@outlook.com.br>
2020-12-01 16:34:41 -0300

    38.20_Exercicio_URI_1066 - Pares, Impares, Positivos e Negativos

---
+++ 04-Estrutura_Condicional/38.20_Exercicio_URI_1066/38.20_Exercicio_URI_1066/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _38._20_Exercicio_URI_1066 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1066
+             */
+
+            int valor, countImpar = 0, countPar = 0, countPositivo = 0, countNegativo = 0;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                countPar = countPar + 1;
+            else
+                countImpar = countImpar + 1;
+
+            if (valor > 0)
+                countPositivo = countPositivo + 1;
+            else if (valor < 0)
+                countNegativo = countNegativo + 1;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                countPar = countPar + 1;
+            else
+                countImpar = countImpar + 1;
+
+            if (valor > 0)
+                countPositivo = countPositivo + 1;
+            else if (valor < 0)
+                countNegativo = countNegativo + 1;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                countPar = countPar + 1;
+            else
+                countImpar = countImpar + 1;
+
+            if (valor > 0)
+                countPositivo = countPositivo + 1;
+            else if (valor < 0)
+                countNegativo = countNegativo + 1;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                countPar = countPar + 1;
+            else
+                countImpar = countImpar + 1;
+
+            if (valor > 0)
+                countPositivo = countPositivo + 1;
+            else if (valor < 0)
+                countNegativo = countNegativo + 1;
+
+            valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0)
+                countPar = countPar + 1;
+            else
+                countImpar = countImpar + 1;
+
+            if (valor > 0)
+                countPositivo = countPositivo + 1;
+            else if (valor < 0)
+                countNegativo = countNegativo + 1;
+
+            Console.WriteLine(countPar + " valor(es) par(es)");
+            Console.WriteLine(countImpar + " valor(es) impar(es)");
+            Console.WriteLine(countPositivo + " valor(es) positivo(s)");
+            Console.WriteLine(countNegativo + " valor(es) negativo(s)");
+        }
+    }
+}
