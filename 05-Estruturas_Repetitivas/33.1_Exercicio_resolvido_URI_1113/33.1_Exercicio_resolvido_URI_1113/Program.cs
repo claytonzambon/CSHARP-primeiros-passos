2999ed5458f1a1b2488a0f56be8e52f2c087fb4b
Clayton Zambon <zambon@outlook.com.br>
2020-12-02 11:34:01 -0300

    URI - 1113 - Crescente e Descrescente

---
+++ 05-Estruturas_Repetitivas/33.1_Exercicio_resolvido_URI_1113/33.1_Exercicio_resolvido_URI_1113/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _33._1_Exercicio_resolvido_URI_1113 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1113
+             */
+
+            int n1, n2;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            n1 = int.Parse(vet[0]);
+            n2 = int.Parse(vet[1]);
+
+            while (n1 != n2) {
+                if (n1 < n2) {
+                    Console.WriteLine("Crescente");
+                }
+                else {
+                    Console.WriteLine("Decrescente");
+                }
+
+                vet = Console.ReadLine().Split(' ');
+                n1 = int.Parse(vet[0]);
+                n2 = int.Parse(vet[1]);
+            }
+        }
+    }
+}
---
+++ 05-Estruturas_Repetitivas/40.1_Estrutura_repetitiva_enquanto/40.1_Estrutura_repetitiva_enquanto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _40._1_Estrutura_repetitiva_enquanto {
+    class Program {
+        static void Main(string[] args) {
+            /* Fazer um programa que lê números inteiros até que um zero seja lido. 
+             * Ao final mostra a soma dos números lidos
+             */
+
+            int x, soma;
+
+            x = int.Parse(Console.ReadLine());
+            soma = 0;
+            while (x != 0) {
+                soma = soma + x;
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
