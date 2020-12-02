79425937f0847a8157dc6be50826996c7731ef98
Clayton Zambon <zambon@outlook.com.br>
2020-12-02 12:02:04 -0300

    URI-1114 - Senha Fixa

---
+++ 05-Estruturas_Repetitivas/45.1_URI-1114/45.1_URI-1114/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _45._1_URI_1114 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1114
+             */
+
+            int senha;
+
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002) {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
