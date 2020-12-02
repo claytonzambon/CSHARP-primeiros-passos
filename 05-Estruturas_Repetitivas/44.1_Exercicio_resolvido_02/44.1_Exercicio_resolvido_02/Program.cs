9a5f52780ec82b661f577231cbd4e34e31e1671a
Clayton Zambon <zambon@outlook.com.br>
2020-12-02 11:48:58 -0300

    44.1_Exercicio_resolvido_02

---
+++ 05-Estruturas_Repetitivas/44.1_Exercicio_resolvido_02/44.1_Exercicio_resolvido_02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace _44._1_Exercicio_resolvido_02 {
+    class Program {
+        static void Main(string[] args) {
+
+            double idade, soma, media;
+            int count;
+
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = 0.0;
+            count = 0;
+            while (idade >= 0) {
+                soma = soma + idade;
+                count++;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            if (count == 0) {
+                Console.WriteLine("impossivel calcular");
+            }
+            else {
+                media = soma / count;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ 05-Estruturas_Repetitivas/44.1_Exercicio_resolvido_02/README.md
@@ -0,0 +1,15 @@
+# 44.Exercício Resolvido 02
+##### Faça um programa para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo. O último dado, que não entrará nos cálculos, contém um valor de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos. Se for entrado um valor negativo na primeira vez, mostrar a mensagem "impossível calcular".
+
+Exemplos:
+
+| Entrada:  | Saída:                |
+|-----------|-----------------------|
+| 31        | 34.67                 |
+| 27        |                       |
+| 46        |                       |
+| -5        |                       |
+
+| Entrada:  | Saída:                |
+|-----------|-----------------------|
+| -10       | impossivel calcular   |
