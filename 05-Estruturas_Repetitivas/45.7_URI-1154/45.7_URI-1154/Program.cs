274d8da659a368e1876e51026006508761588edc
Clayton Zambon <zambon@outlook.com.br>
2020-12-02 16:31:09 -0300

    URI-1154 - Idades

---
+++ 05-Estruturas_Repetitivas/45.7_URI-1154/45.7_URI-1154/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace _45._7_URI_1154 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1154
+             */
+
+            int idade, somaIdades = 0, countIdades = 0;
+            double media;
+
+            idade = int.Parse(Console.ReadLine());
+            while (idade > 0) {
+                somaIdades = somaIdades + idade;
+                countIdades++;
+                idade = int.Parse(Console.ReadLine());
+            }
+
+            media = (double)somaIdades / countIdades;
+            Console.WriteLine(media.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
