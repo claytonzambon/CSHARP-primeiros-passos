39edcdce2de5916fc2905ce86c5524e2b7240b83
Clayton Zambon <zambon@outlook.com.br>
2020-12-02 12:11:40 -0300

    URI-1115 - Quadrante

---
+++ 05-Estruturas_Repetitivas/45.2_URI-1115/45.2_URI-1115/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _45._2_URI_1115 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1115
+             */
+
+            int x, y;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            while (x != 0 && y != 0) {
+                if (x > 0 && y > 0) {
+                    Console.WriteLine("primeiro");
+                }
+                else if (x < 0 && y > 0) {
+                    Console.WriteLine("segundo");
+                }
+                else if (x < 0 && y < 0) {
+                    Console.WriteLine("terceiro");
+                }
+                else {
+                    Console.WriteLine("quarto");
+                }
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+            }
+        }
+    }
+}
