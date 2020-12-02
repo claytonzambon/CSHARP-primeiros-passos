e72f7a447592c85d5bdf22438174d908aa5780a7
Clayton Zambon <zambon@outlook.com.br>
2020-12-02 16:49:37 -0300

    URI-1159 - Soma de Pares Consecutivos

---
+++ 05-Estruturas_Repetitivas/45.8_URI-1159/45.8_URI-1159/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _45._8_URI_1159 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1159
+             */
+
+            int x, soma;
+
+            x = int.Parse(Console.ReadLine());
+            while (x != 0) {
+                if (x % 2 != 0) {
+                    x = x + 1;
+                }
+                soma = x + x + 2 + x + 4 + x + 6 + x + 8;
+                Console.WriteLine(soma);
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
