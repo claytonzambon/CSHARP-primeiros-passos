c5c9eb9ec55f037c4dc0f69877283f50560e5a87
Clayton Zambon <zambon@outlook.com.br>
2020-12-03 15:41:32 -0300

    46.2_Estrutura_repetitiva_para

---
+++ 05-Estruturas_Repetitivas/46.2_Estrutura_repetitiva_para/46.2_Estrutura_repetitiva_para/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _46._2_Estrutura_repetitiva_para {
+    class Program {
+        static void Main(string[] args) {
+
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("Valor de i: " + i);
+            }
+
+            Console.WriteLine("\n\n");
+
+            for (int i = 4; i >= 0; i--) {
+                Console.WriteLine("Valor de i: " + i);
+            }
+        }
+    }
+}
