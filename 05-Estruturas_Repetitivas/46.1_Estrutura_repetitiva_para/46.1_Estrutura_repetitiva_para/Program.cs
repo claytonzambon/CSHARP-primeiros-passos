d98e209ee0c61da4d950df2183a8f25b50e25694
Clayton Zambon <zambon@outlook.com.br>
2020-12-03 15:39:33 -0300

    46.1_Estrutura_repetitiva_para

---
+++ 05-Estruturas_Repetitivas/46.1_Estrutura_repetitiva_para/46.1_Estrutura_repetitiva_para/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _46._1_Estrutura_repetitiva_para {
+    class Program {
+        static void Main(string[] args) {
+
+            int N, x, soma;
+
+            N = int.Parse(Console.ReadLine());
+
+            soma = 0;
+            for (int i = 0; i < N; i++) {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
---
+++ 05-Estruturas_Repetitivas/46.1_Estrutura_repetitiva_para/README.md
@@ -0,0 +1,11 @@
+# 46.Estrutura repetitiva PARA
+##### Fazer um programa que lê um valor inteiro N e depois N números inteiros. Ao final, mostra a soma dos N números lidos.
+
+Exemplos:
+
+| Entrada: | Saída:             |
+|----------|--------------------|
+| 3        | 11                 |
+| 5        |                    |
+| 2        |                    |
+| 4        |                    |
