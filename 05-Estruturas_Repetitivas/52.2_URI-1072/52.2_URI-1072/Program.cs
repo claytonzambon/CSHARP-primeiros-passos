c1e56fcc2a21fd29cf05d01d5c7a842e389af4bf
Clayton Zambon <zambon@outlook.com.br>
2020-12-05 00:38:55 -0300

    URI-1072 - Intervalo 2

---
+++ 05-Estruturas_Repetitivas/52.2_URI-1072/52.2_URI-1072/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _52._2_URI_1072 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1072
+             */
+
+            int N, X, countIn, countOut;
+
+            N = int.Parse(Console.ReadLine());
+
+            countIn = 0;
+            countOut = 0;
+            for (int i = 0; i < N; i++) {
+                X = int.Parse(Console.ReadLine());
+                if (X >= 10 && X <= 20)
+                    countIn++;
+                else
+                    countOut++;
+            }
+            Console.WriteLine(countIn + " in");
+            Console.WriteLine(countOut + " out");
+        }
+    }
+}
