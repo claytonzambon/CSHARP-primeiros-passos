2c1659d783c44bdbfc7909a6eae070901276fa74
Clayton Zambon <zambon@outlook.com.br>
2020-12-05 14:43:57 -0300

    URI-1080 - Maior e Posicao

---
+++ 05-Estruturas_Repetitivas/52.7_URI-1080/52.7_URI-1080/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _52._7_URI_1080 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1080
+             */
+
+            int valor, maior = 0, posicaoMaior = 0;
+
+            for (int i = 1; i <= 100; i++) {
+                valor = int.Parse(Console.ReadLine());
+                if (valor > maior) {
+                    maior = valor;
+                    posicaoMaior = i;
+                }
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicaoMaior);
+        }
+    }
+}
