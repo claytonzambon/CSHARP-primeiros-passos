ada6d881d350f0ad01a23b2ba7b6f6dbec3e2158
Clayton Zambon <zambon@outlook.com.br>
2020-12-05 00:30:53 -0300

    URI-1067 - Numeros Impares

---
+++ 05-Estruturas_Repetitivas/52.1_URI-1067/52.1_URI-1067/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _52._1_URI_1067 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1067
+             */
+
+            int X;
+
+            X = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= X; i++) {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
