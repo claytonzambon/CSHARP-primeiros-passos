2cc5389968c42d6c89abe24a4f2539aa5058b4f9
Clayton Zambon <zambon@outlook.com.br>
2020-12-05 15:36:11 -0300

    URI-1096 - Sequencia IJ 2

---
+++ 05-Estruturas_Repetitivas/52.10_URI-1096/52.10_URI-1096/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _52._10_URI_1096 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1096
+             */
+
+            for (int i = 1; i <= 9; i+=2) {
+                for (int j = 7; j >= 5; j--) {
+                    Console.WriteLine("I=" + i + " J=" + j);
+                }
+            }
+        }
+    }
+}
