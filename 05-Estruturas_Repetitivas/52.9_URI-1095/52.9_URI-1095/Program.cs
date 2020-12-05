39906593ddf25021aaa6bb9cd4025a2364d164de
Clayton Zambon <zambon@outlook.com.br>
2020-12-05 15:21:17 -0300

    URI-1095 - Sequencia IJ 1

---
+++ 05-Estruturas_Repetitivas/52.9_URI-1095/52.9_URI-1095/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _52._9_URI_1095 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1095
+             */
+
+            int i = 1;
+            int j = 60;
+
+            while (j >= 0) {
+                Console.WriteLine("I=" + i + " J=" + j);
+                i += 3;
+                j -= 5;
+            }
+        }
+    }
+}
