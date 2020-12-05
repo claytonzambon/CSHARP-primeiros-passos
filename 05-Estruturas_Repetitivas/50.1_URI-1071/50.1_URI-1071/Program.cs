2740c8ca698bea42fa929ec5d134fb56ffee3fb8
Clayton Zambon <zambon@outlook.com.br>
2020-12-05 00:01:38 -0300

    URI-1071 - Soma de Impares Consecutivos I

---
+++ 05-Estruturas_Repetitivas/50.1_URI-1071/50.1_URI-1071/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _50._1_URI_1071 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1071
+             */
+
+            int x, y, menor, maior, soma;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y) {
+                menor = x;
+                maior = y;
+            }
+            else {
+                menor = y;
+                maior = x;
+            }
+
+            soma = 0;
+            for (int i = menor + 1; i < maior; i++) {
+                if (i % 2 != 0) {
+                    soma = soma + i;
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
