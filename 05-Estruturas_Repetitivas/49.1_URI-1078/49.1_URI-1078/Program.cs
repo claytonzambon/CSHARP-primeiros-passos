238c72005b3d11b84e94ca181fb6809c44ca295a
Clayton Zambon <zambon@outlook.com.br>
2020-12-04 23:52:42 -0300

    URI-1078 - Tabuada

---
+++ 05-Estruturas_Repetitivas/49.1_URI-1078/49.1_URI-1078/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _49._1_URI_1078 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1078
+             */
+
+            int N, produto;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++) {
+                produto = i * N;
+                Console.WriteLine(i + " x " + N + " = " + produto);
+            }
+        }
+    }
+}
