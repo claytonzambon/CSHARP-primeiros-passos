7a0d1d77275d4985929c8b717076f37c7aae8394
Clayton Zambon <zambon@outlook.com.br>
2020-12-05 14:27:19 -0300

    URI-1075 - Resto 2

---
+++ 05-Estruturas_Repetitivas/52.5_URI-1075/52.5_URI-1075/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _52._5_URI_1075 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1075
+             */
+
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10000; i++) {
+                if (i % N == 2) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
