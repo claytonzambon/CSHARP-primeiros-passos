c14bd7df755868755902797df696ae3f4dd3667d
Clayton Zambon <zambon@outlook.com.br>
2020-12-05 15:41:56 -0300

    URI-1097 - Sequencia IJ 3

---
+++ 05-Estruturas_Repetitivas/52.11_URI-1097/52.11_URI-1097/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _52._11_URI_1097 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1097
+             */
+
+            for (int i = 1; i <= 9; i += 2) {
+                for (int j = i + 6; j >= i + 4; j--) {
+                    Console.WriteLine("I=" + i + " J=" + j);
+                }
+            }
+        }
+    }
+}
