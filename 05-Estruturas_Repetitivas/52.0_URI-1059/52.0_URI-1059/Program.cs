15e0df0b33c24a146769daa1984165fa82e07478
Clayton Zambon <zambon@outlook.com.br>
2020-12-05 00:47:33 -0300

    URI-1059 - Numeros Pares

---
+++ 05-Estruturas_Repetitivas/52.0_URI-1059/52.0_URI-1059/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _52._0_URI_1059 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1059
+             */
+
+            for (int i = 1; i <= 100; i++) {
+                if (i % 2 == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
