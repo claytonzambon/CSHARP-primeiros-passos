c591b80a956832151952008ba541ad03baea11f2
Clayton Zambon <zambon@outlook.com.br>
2020-12-07 09:47:28 -0300

    URI-1099 - Soma de Impares Consecutivos II

---
+++ 05-Estruturas_Repetitivas/52.13_URI-1099/52.13_URI-1099/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _52._13_URI_1099 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1099
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                int X = int.Parse(vet[0]);
+                int Y = int.Parse(vet[1]);
+                
+                if (X > Y) {
+                    int aux = X;
+                    X = Y;
+                    Y = aux;
+                }
+
+                int soma = 0;
+                for (int j = X + 1; j < Y; j++) {
+                    if (j % 2 != 0) {
+                        soma += j;
+                    }
+                }
+                Console.WriteLine(soma);
+            }
+        }
+    }
+}
