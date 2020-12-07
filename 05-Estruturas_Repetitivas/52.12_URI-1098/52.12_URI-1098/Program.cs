7c3686b67fe35171af5b292bf11534b688682cdb
Clayton Zambon <zambon@outlook.com.br>
2020-12-07 09:27:35 -0300

    URI-1098 - Sequencia IJ 4

---
+++ 05-Estruturas_Repetitivas/52.12_URI-1098/52.12_URI-1098/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace _52._12_URI_1098 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1098
+             */
+
+            for (double i = 0; i <= 2; i = i + 0.2) {
+                for (double j = 1; j <= 3; j++) {
+                    Console.WriteLine("I=" + i.ToString("", CultureInfo.InvariantCulture) + 
+                        " J=" + (j + i).ToString("", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
