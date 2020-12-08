d8346551b99e8e0e22d72cd6a79d3fa88d649955
Clayton Zambon <zambon@outlook.com.br>
2020-12-08 12:04:41 -0300

    URI-1143 - Quadrado e ao Cubo

---
+++ 05-Estruturas_Repetitivas/52.19_URI-1143/52.19_URI-1143/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _52._19_URI_1143 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1143
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) {
+                int produto = 1;
+                Console.Write((produto *= i) + " ");
+                Console.Write((produto *= i) + " ");
+                Console.WriteLine(produto *= i);
+            }
+        }
+    }
+}
