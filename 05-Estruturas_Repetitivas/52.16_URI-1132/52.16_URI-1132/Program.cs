28e841209bb1212b0f44d3e256e2b94d62b4b045
Clayton Zambon <zambon@outlook.com.br>
2020-12-08 09:43:59 -0300

    URI-1132 - Multiplos de 13

---
+++ 05-Estruturas_Repetitivas/52.16_URI-1132/52.16_URI-1132/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _52._16_URI_1132 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1132
+             */
+
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+
+            if (X > Y) {
+                int aux = X;
+                X = Y;
+                Y = aux;
+            }
+
+            int soma = 0;
+            for (int i = X; i <= Y; i++) {
+                if (i % 13 != 0) {
+                    soma += i;
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
