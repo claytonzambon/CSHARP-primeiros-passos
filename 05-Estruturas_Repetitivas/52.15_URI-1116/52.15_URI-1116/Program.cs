c96703dce04ec9f4d60866259aa42321e42c8dac
Clayton Zambon <zambon@outlook.com.br>
2020-12-08 09:34:33 -0300

    URI-1116 - Dividindo X por Y

---
+++ 05-Estruturas_Repetitivas/52.15_URI-1116/52.15_URI-1116/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace _52._15_URI_1116 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1116
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                int X = int.Parse(vet[0]);
+                int Y = int.Parse(vet[1]);
+                if (Y != 0) {
+                    Console.WriteLine(((double)X / Y).ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else {
+                    Console.WriteLine("divisao impossivel");
+                }
+            }
+        }
+    }
+}
