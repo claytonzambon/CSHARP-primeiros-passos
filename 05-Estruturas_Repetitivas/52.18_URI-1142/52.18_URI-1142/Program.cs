3ab4f53fc1fc291253a03f887fe426d31fb895e3
Clayton Zambon <zambon@outlook.com.br>
2020-12-08 11:52:37 -0300

    URI-142 - PUM

---
+++ 05-Estruturas_Repetitivas/52.18_URI-1142/52.18_URI-1142/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _52._18_URI_1142 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1142
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            int primeiro = 1;
+            for (int i = 1; i <= N; i++) {
+
+                int segundo = primeiro + 1;
+                int terceiro = primeiro + 2;
+                Console.WriteLine(primeiro + " " + segundo + " " + terceiro + " PUM");
+
+                primeiro += 4;
+            }
+        }
+    }
+}
