d2c5ed031728b2663136b6a2c0d530572a59fee3
Clayton Zambon <zambon@outlook.com.br>
2020-12-08 12:24:52 -0300

    URI-1144 - Sequencia Logica

---
+++ 05-Estruturas_Repetitivas/52.20_URI-1144/52.20_URI-1144/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _52._20_URI_1144 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1144
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) {
+
+                int prodSegundo = i * i;
+                int prodTerceiro = i * i * i;
+
+                Console.WriteLine(i + " " + prodSegundo + " " + prodTerceiro);
+                Console.WriteLine(i + " " + (prodSegundo + 1) + " " + (prodTerceiro + 1));
+            }
+        }
+    }
+}
