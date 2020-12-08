3efb69eaade2de28b46e709ff800f46f35b873ec
Clayton Zambon <zambon@outlook.com.br>
2020-12-08 09:56:24 -0300

    URI-1133 - Resto da Divisao

---
+++ 05-Estruturas_Repetitivas/52.17_URI-1133/52.17_URI-1133/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _52._17_URI_1133 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1133
+             */
+
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+
+            if (X > Y) {
+                int aux = X;
+                X = Y;
+                Y = aux;
+            }
+
+            for (int i = X + 1; i < Y; i++) {
+                if (i % 5 == 2 || i % 5 == 3) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
