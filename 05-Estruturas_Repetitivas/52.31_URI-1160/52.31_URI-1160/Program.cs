83e1b5afb876ebba22cefe3dc8d16d3be20c342f
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 15:30:09 -0300

    URI-1160 - Crescimento Populacional

---
+++ 05-Estruturas_Repetitivas/52.31_URI-1160/52.31_URI-1160/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _52._31_URI_1160 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1160
+             */
+
+            int T, PA, PB, crescA, crescB, anos;
+            double G1, G2;
+            string[] vet;
+
+            T = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= T; i++) {
+                vet = Console.ReadLine().Split(' ');
+                PA = int.Parse(vet[0]);
+                PB = int.Parse(vet[1]);
+                G1 = double.Parse(vet[2]);
+                G2 = double.Parse(vet[3]);
+
+                crescA = PA;
+                crescB = PB;
+
+                anos = 0;
+
+                while (crescA <= crescB) {
+                    crescA += (int)(crescA * (G1 / 100));
+                    crescB += (int)(crescB * (G2 / 100));
+                    anos += 1;
+                }
+
+                if (anos > 100) {
+                    Console.WriteLine("Mais de 1 seculo.");
+                }
+                else {
+                    Console.WriteLine(anos + " anos.");
+                }
+            }
+        }
+    }
+}
