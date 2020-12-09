304afc9fa406d64ecdd996f1c8f6fdf9f19a0049
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 13:56:31 -0300

    URI-1151 - Fibonacci Facil

---
+++ 05-Estruturas_Repetitivas/52.25_URI-1151/52.25_URI-1151/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _52._25_URI_1151 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1151
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < N; i++) {
+                if (i == N - 1) {
+                    Console.WriteLine(a);
+                }
+                else {
+                    Console.Write(a + " ");
+                }
+                int aux = b;
+                b = a;
+                a = aux + b;
+            }
+        }
+    }
+}
