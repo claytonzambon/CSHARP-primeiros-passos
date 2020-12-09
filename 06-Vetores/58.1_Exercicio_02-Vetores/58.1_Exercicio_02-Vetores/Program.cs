d31406704372f3476e5d704ac8ede28f28630c6a
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 17:12:13 -0300

    58.1-Exercicio-02 - Vetores

---
+++ 06-Vetores/58.1_Exercicio_02-Vetores/58.1_Exercicio_02-Vetores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace _58._1_Exercicio_02_Vetores {
+    class Program {
+        static void Main(string[] args) {
+
+            int N;
+            double soma, media;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++) {
+                vet[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < N; i++) {
+                Console.Write(vet[i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+
+            soma = 0.0;
+            media = 0.0;
+            for (int i = 0; i < N; i++) {
+                soma += vet[i];
+            }
+            media = soma / N;
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ 06-Vetores/58.1_Exercicio_02-Vetores/README.md
@@ -0,0 +1,12 @@
+# 58.1.Exercicio Resolvido 02 - Vetores
+##### Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida:
+##### - Imprimir todos os elementos do vetor
+##### - Mostrar na tela a soma e a média dos elementos do vetor
+
+Exemplos:
+
+| Entrada:          | Saída:            |
+|-------------------|-------------------|
+| 4                 | 8.0 4.0 10.0 14.0 |
+| 8.0 4.0 10.0 14.0 | 36.00             |
+|                   | 9.00              |
