8d844f94e32c188bf82a58415b3b58a6a0b057aa
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 14:29:52 -0300

    URI-1155 - Sequencia S

---
+++ 05-Estruturas_Repetitivas/52.27_URI-1155/52.27_URI-1155/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace _52._27_URI_1155 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1155
+             */
+
+            double S = 0;
+            for (int i = 1; i <= 100; i++) {
+                S += 1.0 / i;
+            }
+            Console.WriteLine(S.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
