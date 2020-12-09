4ebc538df804f99f358c2f3025ec9f0da9fe5b9e
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 15:38:45 -0300

    URI-1164 - Numero Perfeito

---
+++ 05-Estruturas_Repetitivas/52.32_URI-1164/52.32_URI-1164/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _52._32_URI_1164 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1164
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++) {
+                int X = int.Parse(Console.ReadLine());
+                int soma = 0;
+                for (int j = 1; j < X; j++) {
+                    if (X % j == 0) {
+                        soma += j;
+                    }
+                }
+                if (soma == X) {
+                    Console.WriteLine(X + " eh perfeito");
+                }
+                else {
+                    Console.WriteLine(X + " nao eh perfeito");
+                }
+            }
+        }
+    }
+}
