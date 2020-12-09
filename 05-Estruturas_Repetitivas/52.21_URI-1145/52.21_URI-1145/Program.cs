bd451a866fc7ef6769949f753631c3b0d87645ac
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 11:45:34 -0300

    URI-1145 - Sequencia Logica 2

---
+++ 05-Estruturas_Repetitivas/52.21_URI-1145/52.21_URI-1145/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _52._21_URI_1145 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1145
+             */
+
+            string[] vet = Console.ReadLine().Split(' ');
+            int X = int.Parse(vet[0]);
+            int Y = int.Parse(vet[1]);
+
+            for (int i = 1; i <= Y; i++) {
+                Console.Write(i);
+                if (i % X == 0) {
+                    Console.WriteLine();
+                }
+                else {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
