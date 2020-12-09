b672eb15408c11922b85dbf2ff765339266b3299
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 11:53:02 -0300

    URI-1146 - Sequencias Crescentes

---
+++ 05-Estruturas_Repetitivas/52.22_URI-1146/52.22_URI-1146/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _52._22_URI_1146 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1146
+             */
+            int X;
+
+            do {
+                X = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= X; i++) {
+                    Console.Write(i);
+                    if (i % X == 0) {
+                        Console.WriteLine();
+                    }
+                    else {
+                        Console.Write(" ");
+                    }
+                }
+
+            } while (X != 0);
+        }
+    }
+}
