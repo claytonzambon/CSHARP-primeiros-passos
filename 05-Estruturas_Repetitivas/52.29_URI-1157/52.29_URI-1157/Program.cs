9ec3fd499debc68066e70be7bf29c60a2154c729
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 14:42:24 -0300

    URI-1157 - Divisores I

---
+++ 05-Estruturas_Repetitivas/52.29_URI-1157/52.29_URI-1157/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _52._29_URI_1157 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1157
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) {
+                if (N % i == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
