1773266c48921825b22275dea0365153cca078ca
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 12:21:35 -0300

    URI-1150 - Ultrapassando Z

---
+++ 05-Estruturas_Repetitivas/52.24_URI-1150/52.24_URI-1150/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _52._24_URI_1150 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1150
+             */
+
+            int X, Z, count, soma;
+
+            X = int.Parse(Console.ReadLine());
+            Z = int.Parse(Console.ReadLine());
+
+            while (Z <= X) {
+                Z = int.Parse(Console.ReadLine());
+            }
+
+            count = 1;
+            soma = X;
+            do {
+                X += 1;
+                soma += X;
+                count += 1;
+            } while (soma <= Z);
+
+            Console.WriteLine(count);
+        }
+    }
+}
