bbe577024c9fdbb24de416dff0d19b3ff6b1ebac
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 16:26:06 -0300

    54.1 - Vetores

---
+++ 06-Vetores/54.1_Vetores/54.1_Vetores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace _54._1_Vetores {
+    class Program {
+        static void Main(string[] args) {
+            int N;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+
+            for (int i = 0; i < N; i++) {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < N; i++) {
+                Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ 06-Vetores/54.1_Vetores/README.md
@@ -0,0 +1,12 @@
+# 54.1.Vetores
+##### Fazer um programa para ler um número inteiro positivo N, depois ler N números quaisquer e armazená-los em um vetor. Em seguida, mostrar na tela todos elementos do vetor
+
+Exemplos:
+
+| Entrada:  | Saída:    |
+|-----------|-----------|
+| 4         | 10.5      |
+| 10.5      | 4.2       |
+| 4.2       | -7.1      |
+| -7.1      | 15.0      |
+| 15.0      |           |
