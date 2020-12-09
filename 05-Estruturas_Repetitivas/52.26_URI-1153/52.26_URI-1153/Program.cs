1b5ef8be47345b8ad39254f6581f796ecba3e5c9
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 14:05:44 -0300

    URI-1153 - Fatorial Simples

---
+++ 05-Estruturas_Repetitivas/52.26_URI-1153/52.26_URI-1153/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _52._26_URI_1153 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1153
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            int fatorial = 1;
+            for (int i = 1; i <= N; i++) {
+                fatorial = fatorial * i;
+            }
+            Console.WriteLine(fatorial);
+        }
+    }
+}
