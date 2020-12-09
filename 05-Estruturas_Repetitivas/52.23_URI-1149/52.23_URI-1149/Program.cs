d254ab4c16d9bd5480b0ce5f0f3a6ef9fc5c575c
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 12:07:04 -0300

    URI-1149 - Somando Inteiros Consecutivos

---
+++ 05-Estruturas_Repetitivas/52.23_URI-1149/52.23_URI-1149/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _52._23_URI_1149 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1149
+             */
+
+            int A, N, count, soma;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            A = int.Parse(vet[0]);
+
+            count = 1;
+            N = int.Parse(vet[count]);
+            while (N <= 0) {
+                count += 1;
+                N = int.Parse(vet[count]);
+            }
+
+            soma = 0;
+            for (int i = 0; i < N; i++) {
+                soma += A + i;
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
