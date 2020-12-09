48c3eaec9907d78bf30ac5b408bbb00cc1a986d6
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 15:45:28 -0300

    URI-1165 - Numero Primo

---
+++ 05-Estruturas_Repetitivas/52.33_URI-1165/52.33_URI-1165/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _52._33_URI_1165 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1165
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++) {
+                int X = int.Parse(Console.ReadLine());
+                int count = 0;
+                for (int j = 1; j <= X; j++) {
+                    if (X % j == 0) {
+                        count++;
+                    }
+                }
+                if (count == 2) {
+                    Console.WriteLine(X + " eh primo");
+                }
+                else {
+                    Console.WriteLine(X + " nao eh primo");
+                }
+            }
+        }
+    }
+}
