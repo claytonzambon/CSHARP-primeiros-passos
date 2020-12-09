df1a198e39d9b9f4112641906d882170ea13882b
Clayton Zambon <zambon@outlook.com.br>
2020-12-09 14:38:58 -0300

    URI-1156 - Sequencia S II

---
+++ 05-Estruturas_Repetitivas/52.28_URI-1156/52.28_URI-1156/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace _52._28_URI_1156 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1156
+             */
+
+            double denominador = 1.0, soma = 0.0;
+
+            for (int i = 1; i <= 39; i += 2) {
+                soma += i / denominador;
+                denominador *= 2.0;
+            }
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
