1d74e687c98d53b2a57d0c29afa9ffe13980c339
Clayton Zambon <zambon@outlook.com.br>
2020-12-10 15:08:57 -0300

    Exercicio Vetores - 02

---
+++ 06-Vetores/61.2_Exercicio_Vetores_02/61.2_Exercicio_Vetores_02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _61._2_Exercicio_Vetores_02 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            int[] inteiros = new int[N];
+
+            string[] vet = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++) {
+                inteiros[i] = int.Parse(vet[i]);
+            }
+
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                if (inteiros[i] % 2 == 0) {
+                    Console.Write(inteiros[i] + " ");
+                    count++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ 06-Vetores/61.2_Exercicio_Vetores_02/README.md
@@ -0,0 +1,11 @@
+# 61.2.Exercicio de Vetores - 2
+##### Faça um programa que leia N números inteiros e armazene-os em um vetor. Em seguida, mostre na tela:
+##### - todos os números pares
+##### - a quantidade de números pares
+
+Exemplos:
+
+| Entrada:          | Saída:    |
+|-------------------|-----------|
+| 6                 | 8 2 14 20 |
+| 8 2 11 14 13 20   | 4         |
