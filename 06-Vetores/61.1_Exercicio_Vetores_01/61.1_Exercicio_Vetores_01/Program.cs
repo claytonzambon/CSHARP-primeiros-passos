042047e7915840ed892fe7492ad40586d7ab4849
Clayton Zambon <zambon@outlook.com.br>
2020-12-10 14:52:33 -0300

    Exericio Vetores - 01

---
+++ 06-Vetores/61.1_Exercicio_Vetores_01/61.1_Exercicio_Vetores_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace _61._1_Exercicio_Vetores_01 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            double[] numeros = new double[N];
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            //Lendo os numeros
+            for (int i = 0; i < N; i++) {
+                numeros[i] = double.Parse(vet[i], CultureInfo.InvariantCulture);
+            }
+
+            double maior = numeros[0];
+            int posicao = 0;
+            for (int i = 0; i < N; i++) {
+                if (maior < numeros[i]) {
+                    maior = numeros[i];
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+        }
+    }
+}
