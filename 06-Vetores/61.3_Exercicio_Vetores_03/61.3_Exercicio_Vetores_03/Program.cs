e0546ce4e7cf95e823575ae2e747329b682b3970
Clayton Zambon <zambon@outlook.com.br>
2020-12-10 15:19:22 -0300

    Exercicio Vetores - 03

---
+++ 06-Vetores/61.3_Exercicio_Vetores_03/61.3_Exercicio_Vetores_03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _61._3_Exercicio_Vetores_03 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            int[] A = new int[N];
+            int[] B = new int[N];
+            int[] C = new int[N];
+
+            //Lendo valores para o Vetor A
+            string[] vet = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++) {
+                A[i] = int.Parse(vet[i]);
+            }
+
+            //Lendo valores para o Vetor B
+            vet = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++) {
+                B[i] = int.Parse(vet[i]);
+            }
+
+            //Populando o Vetor C
+            for (int i = 0; i < N; i++) {
+                C[i] = A[i] + B[i];
+            }
+
+            //Exibindo o Vetor C
+            for (int i = 0; i < N; i++) {
+                Console.Write(C[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 06-Vetores/61.3_Exercicio_Vetores_03/README.md
@@ -0,0 +1,10 @@
+# 61.3.Exercicio de Vetores - 3
+##### Faça um programa para ler dois vetores A e B, contendo N elementos cada. Em seguida, gere um terceiro vetor C onde cada elemento de C é a soma dos elementos correspondentes de A e B. Imprima o vetor C gerado.
+
+Exemplos:
+
+| Entrada:          | Saída:            |
+|-------------------|-------------------|
+| 6                 | 13 12 14 15 23 27 |
+| 8 2 11 14 13 20   |
+| 5 10 3 1 10 7     |
