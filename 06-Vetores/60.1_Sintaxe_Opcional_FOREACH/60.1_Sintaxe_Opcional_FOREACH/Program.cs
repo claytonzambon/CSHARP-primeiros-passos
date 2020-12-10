c2c403de4dc94d9bfc969e778206ef16b2ba3b2b
Clayton Zambon <zambon@outlook.com.br>
2020-12-10 14:33:54 -0300

    60.1_Sintaxe_Opcional_FOREACH

---
+++ 06-Vetores/60.1_Sintaxe_Opcional_FOREACH/60.1_Sintaxe_Opcional_FOREACH/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _60._1_Sintaxe_Opcional_FOREACH {
+    class Program {
+        static void Main(string[] args) {
+
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string[] vet = new string[N];
+
+            for (int i = 0; i < N; i++) {
+                vet[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Nomes lidos:");
+            foreach (string elemento in vet) {
+                Console.WriteLine(elemento);
+            }
+        }
+    }
+}
---
+++ 06-Vetores/60.1_Sintaxe_Opcional_FOREACH/README.md
@@ -0,0 +1,11 @@
+# 60.1.Sintaxe Opcional - FOREACH
+##### Fazer um programa para ler um valor N e depois o nome de N pessoas. Em seguida, mostrar os nomes lidos.
+
+Exemplos:
+
+| Entrada:  | Saída:        |
+|-----------|---------------|
+| 3         | Nomes Lidos:  |
+| Maria     | Maria         |
+| Joaquim   | Joaquim       |
+| Ana       | Ana           |
