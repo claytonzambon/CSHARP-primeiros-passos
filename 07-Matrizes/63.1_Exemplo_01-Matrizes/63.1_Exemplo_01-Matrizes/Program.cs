34521dddf5f0405485fb24f31e50a4a28298d366
Clayton Zambon <zambon@outlook.com.br>
2020-12-11 10:57:23 -0300

    Exemplo 01 - Matrizes

---
+++ 07-Matrizes/63.1_Exemplo_01-Matrizes/63.1_Exemplo_01-Matrizes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _63._1_Exemplo_01_Matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+            //Declarar variáveis para ler o tamanho da matriz
+            int M, N;
+            
+            string[] vet1 = Console.ReadLine().Split(' ');
+            M = int.Parse(vet1[0]);
+            N = int.Parse(vet1[1]);
+
+            //Declarar e instanciar uma matriz de tamanho M, N
+            int[,] A = new int[M, N];
+
+            //Ler dados do teclado e preencher a matriz
+            for (int i = 0; i < M; i++) {
+                string[] vet2 = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    A[i, j] = int.Parse(vet2[j]);
+                }
+            }
+
+            //Percorrer e exibir a matriz
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    Console.Write(A[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 07-Matrizes/63.1_Exemplo_01-Matrizes/README.md
@@ -0,0 +1,10 @@
+# 63.1.Exemplo - 01 Matrizes
+##### Fazer um programa para ler dois números inteiros positivos M e N, depois ler uma matriz de M linhas e N colunas contendo números inteiros. Em seguida, mostrar na tela a matriz lida conforme exemplo.
+
+Exemplos:
+
+| Entrada:  | Saída:    |
+|-----------|-----------|
+| 2 3       | 6 3 10    |
+| 6 3 10    | 8 12 5    |
+| 8 12 5    |
