65a7779f9a55474f633ecd5cac80d569ec125a56
Clayton Zambon <zambon@outlook.com.br>
2020-12-10 23:31:11 -0300

    Exercicio Vetores - 06

---
+++ 06-Vetores/61.6_Exercicio_Vetores_06/61.6_Exercicio_Vetores_06/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _61._6_Exercicio_Vetores_06 {
+    class Program {
+        static void Main(string[] args) {
+
+            //Ler a quantidade de elementos que terá o Vetor
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            //Declarar Vetor de String e inteiros com N elementos
+            string[] nome = new string[N];
+            int[] idade = new int[N];
+
+            //Ler dados do Teclado e popular os vetores
+            for (int i = 0; i < N; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                nome[i] = vet[0];
+                idade[i] = int.Parse(vet[1]);
+            }
+
+            //Verificar qual a maior idade
+            int maisVelho = 0;
+            string nomeVelho = " ";
+            for (int i = 0; i < N; i++) {
+                if (maisVelho < idade[i]) {
+                    maisVelho = idade[i];
+                    nomeVelho = nome[i];
+                }
+            }
+
+            //Exibir o nome da Pessoa mais velha
+            Console.WriteLine("Pessoa mais velha: " + nomeVelho);
+        }
+    }
+}
---
+++ 06-Vetores/61.6_Exercicio_Vetores_06/README.md
@@ -0,0 +1,13 @@
+# 61.6.Exercicio de Vetores - 6
+##### Fazer um programa para ler um conjunto de nomes de pessoas e suas respectivas idades. Depois, mostrar na tela o nome da pessoa mais velha
+
+Exemplos:
+
+| Entrada:  | Saída:                    |
+|-----------|---------------------------|
+| 5         | Pessoa mais velha: Carlos |
+| Joao 16   | 
+| Maria 21  |
+| Teresa 15 |
+| Carlos 23 |
+| Paulo 17  |
