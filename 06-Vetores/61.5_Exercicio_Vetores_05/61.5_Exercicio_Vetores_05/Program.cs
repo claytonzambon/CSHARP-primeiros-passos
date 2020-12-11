72d5a95e202de88a9a59f3bcd7868da4143262f5
Clayton Zambon <zambon@outlook.com.br>
2020-12-10 23:17:01 -0300

    Exercicio Vetores - 05

---
+++ 06-Vetores/61.5_Exercicio_Vetores_05/61.5_Exercicio_Vetores_05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace _61._5_Exercicio_Vetores_05 {
+    class Program {
+        static void Main(string[] args) {
+
+            //Ler a quantidade de elementos do vetor
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            //Declarar um vetor de inteiros de tamanho N
+            int[] inteiros = new int[N];
+
+            //Ler os dados para popular o vetor
+            string[] vet = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++) {
+                inteiros[i] = int.Parse(vet[i]);
+            }
+
+            //Verificar quais valores são PARES, somar e contar
+            int pares = 0, soma = 0;
+            for (int i = 0; i < N; i++) {
+                if (inteiros[i] % 2 == 0) {
+                    soma += inteiros[i];
+                    pares++;
+                }
+            }
+
+            //Calcular e Exibir a média aritmética dos PARES lidos
+            double media = (double) soma / pares;
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ 06-Vetores/61.5_Exercicio_Vetores_05/README.md
@@ -0,0 +1,9 @@
+# 61.5.Exercicio de Vetores - 5
+##### Fazer um programa para ler um vetor de N números inteiros. Em seguida, mostrar na tela a média aritmética somente dos números pares lidos
+
+Exemplos:
+
+| Entrada:          | Saída:  |
+|-------------------|---------|
+| 6                 | 11.0    |
+| 8 2 11 14 13 20   |         |
