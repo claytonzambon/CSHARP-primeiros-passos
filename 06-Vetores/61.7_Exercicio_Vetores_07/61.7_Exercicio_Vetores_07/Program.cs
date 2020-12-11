c1f4513dbf3c242bf09c2267a219ce4e3ab4b986
Clayton Zambon <zambon@outlook.com.br>
2020-12-10 23:48:03 -0300

    Exercicio Vetores - 07

---
+++ 06-Vetores/61.7_Exercicio_Vetores_07/61.7_Exercicio_Vetores_07/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace _61._7_Exercicio_Vetores_07 {
+    class Program {
+        static void Main(string[] args) {
+
+            //Ler a quantidade de elementos que terão os vetores
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            //Declarar vetores de string e int
+            string[] nome = new string[N];
+            double[] nota01 = new double[N];
+            double[] nota02 = new double[N];
+
+            //Ler dados do Teclado e popular os vetores
+            for (int i = 0; i < N; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                nome[i] = vet[0];
+                nota01[i] = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                nota02[i] = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            }
+
+            //Exibir alunos aprovados com média maior que 6.0
+            Console.WriteLine("Alunos aprovados:");
+            for (int i = 0; i < N; i++) {
+                double media = (nota01[i] + nota02[i]) / 2;
+                if (media >= 6.0) {
+                    Console.WriteLine(nome[i]);
+                }
+            }
+
+        }
+    }
+}
---
+++ 06-Vetores/61.7_Exercicio_Vetores_07/README.md
@@ -0,0 +1,12 @@
+# 61.7.Exercicio de Vetores - 7
+##### Fazer um programa para ler um conjunto de N nomes de alunos, bem como as notas que eles tiraram no 1º e 2º semestres. Cada uma dessas informações deve ser armazenada em um vetor. Depois, imprimir os nomes dos alunos aprovados, considerando aprovados aqueles cuja média das notas seja maior ou igual a seis
+
+Exemplos:
+
+| Entrada:      | Saída:            |
+|---------------|-------------------|
+| 4             | Alunos aprovados: |
+| Joao 7.0 8.5  | Joao              |
+| Maria 9.2 6.5 | Maria             |
+| Carlos 5.0 6.0| Teresa            |
+| Teresa 5.5 6.5|                   |
