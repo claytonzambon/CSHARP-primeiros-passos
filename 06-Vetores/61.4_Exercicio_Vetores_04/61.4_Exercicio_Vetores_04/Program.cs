3a0b4d66a59a3b911ee919e213cc935b1e4e2f0e
Clayton Zambon <zambon@outlook.com.br>
2020-12-10 23:06:24 -0300

    Exercicio Vetores - 04

---
+++ 06-Vetores/61.4_Exercicio_Vetores_04/61.4_Exercicio_Vetores_04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace _61._4_Exercicio_Vetores_04 {
+    class Program {
+        static void Main(string[] args) {
+
+            //Ler a quantidade de valores que terá o Vetor
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            //Ler dados para popular o vetor
+            double[] numeros = new double[N];
+
+            string[] vet = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++) {
+                numeros[i] = double.Parse(vet[i], CultureInfo.InvariantCulture);
+            }
+
+            //Realizar a Soma dos elementos do vetor populado
+            double soma = 0.0;
+            for (int i = 0; i < N; i++) {
+                soma += numeros[i];
+            }
+
+            //Calculando e exibindo a média
+            double media = soma / N;
+            Console.WriteLine(media.ToString("F3", CultureInfo.InvariantCulture));
+
+
+            //Exibindo todos os valores do vetor que estão abaixo da média
+            for (int i = 0; i < N; i++) {
+                if (numeros[i] < media) {
+                    Console.WriteLine(numeros[i].ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
---
+++ 06-Vetores/61.4_Exercicio_Vetores_04/README.md
@@ -0,0 +1,10 @@
+# 61.4.Exercicio de Vetores - 4
+##### Fazer um programa para ler um vetor de N números reais. Em seguida, mostrar na tela a média aritmética de todos elementos. Depois mostrar todos os elementos do vetor que estejam abaixo da média
+
+Exemplos:
+
+| Entrada:              | Saída:    |
+|-----------------------|-----------|
+| 4                     | 12.125    |
+| 10.0 15.5 13.2 9.8    | 10.0      |
+|                       | 9.8       |
