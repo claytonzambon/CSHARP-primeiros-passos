a9f88682b1d836e9df468cfc142d8e9528672dfb
Clayton Zambon <zambon@outlook.com.br>
2020-12-14 12:10:27 -0300

    68.3-Exercicio de Matrizes

---
+++ 07-Matrizes/68.3_Exercicio-Matrizes/68.3_Exercicio-Matrizes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _68._3_Exercicio_Matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+            //Ler tamanho da Matriz quadrada
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            //Declarar matriz quadrada de tamanho N
+            int[,] matrizQuadrada = new int[N, N];
+
+            //Ler dados e popular matriz
+            for (int i = 0; i < N; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    matrizQuadrada[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            //Exibir o maior elemento de cada linha da matriz
+            for (int i = 0; i < N; i++) {
+                int maior = matrizQuadrada[i, i];
+                for (int j = 0; j < N; j++) {
+                    if (maior < matrizQuadrada[i, j]) {
+                        maior = matrizQuadrada[i, j];
+                    }
+                }
+                Console.WriteLine(maior);
+            }
+        }
+    }
+}
---
+++ 07-Matrizes/68.3_Exercicio-Matrizes/README.md
@@ -0,0 +1,18 @@
+# 68.3.Exercício - Matrizes
+##### Ler um inteiro N e uma matriz quadrada de ordem N. Mostrar qual o maior elemento de cada linha. Suponha não haver empates.
+
+##### Entrada:
+A entrada contém  número N, depois os dados da matriz.
+
+##### Saída:
+A saída contém os números representando o maior elemento de cada linha da matriz.
+
+Exemplos:
+
+| Entrada:  | Saída:    |
+|-----------|-----------|
+| 4         | 12        |
+| 10 5 12 3 | 7         |
+| 4 7 0 6   | 8         |
+| 3 3 8 1   | 15        |
+| 15 13 4 7 |
