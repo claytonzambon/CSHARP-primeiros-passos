19a7b0a6f3076db4dcb04182dc9c84ba0beec919
Clayton Zambon <zambon@outlook.com.br>
2020-12-14 12:00:51 -0300

    68.2-Exercicio de Matrizes

---
+++ 07-Matrizes/68.2_Exercicio-Matrizes/68.2_Exercicio-Matrizes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _68._2_Exercicio_Matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+            //Ler um número N, tamanho da matriz quadrada
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            //Declarar uma matriz quadrada de tamanho N
+            int[,] matriz = new int[N, N];
+
+            //Ler dados da Matriz
+            for (int i = 0; i < N; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    matriz[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            //Exibir a soma de cada linha
+            for (int i = 0; i < N; i++) {
+                int soma = 0;
+                for (int j = 0; j < N; j++) {
+                    soma += matriz[i, j];
+                }
+                Console.WriteLine(soma);
+            }
+        }
+    }
+}
---
+++ 07-Matrizes/68.2_Exercicio-Matrizes/README.md
@@ -0,0 +1,17 @@
+# 68.2.Exercício - Matrizes
+##### Ler um número N e depois uma matriz NxN com números inteiros. Depois, mostrar na tela a soma dos elementos de cada linha da matriz.
+
+##### Entrada:
+A entrada contém  número N, depois os dados da matriz.
+
+##### Saída:
+A saída contém os números representando a soma dos elementos de cada linha da matriz.
+
+Exemplos:
+
+| Entrada:  | Saída:    |
+|-----------|-----------|
+| 3         | 11        |
+| 5 2 4     | 19        |
+| 10 3 6    | 29        |
+| 9 8 12    |           |
