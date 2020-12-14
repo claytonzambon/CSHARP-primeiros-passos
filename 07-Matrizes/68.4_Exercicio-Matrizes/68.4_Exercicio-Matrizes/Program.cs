dea28f33010abda65a1c39a956d82260f5c6a7ac
Clayton Zambon <zambon@outlook.com.br>
2020-12-14 14:03:38 -0300

    68.4-Exercicio de Matrizes

---
+++ 07-Matrizes/68.4_Exercicio-Matrizes/68.4_Exercicio-Matrizes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _68._4_Exercicio_Matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+            //Ler um inteiro N
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            //Declara a matriz quadrada de ordem N
+            int[,] matrizQuadrada = new int[N, N];
+
+            //Ler dados e popular a matris
+            for (int i = 0; i < N; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    matrizQuadrada[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            //Somar valores acima da Diagonal Principal
+            int soma = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j) {
+                        for (int k = j + 1; k < N; k++) {
+                            soma += matrizQuadrada[i, k];
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
---
+++ 07-Matrizes/68.4_Exercicio-Matrizes/README.md
@@ -0,0 +1,17 @@
+# 68.4.Exercício - Matrizes
+##### Ler um inteiro N e uma matriz quadrada de ordem N. Mostrar a soma dos elementos acima da diagonal principal.
+
+##### Entrada:
+A entrada contém  número N, depois os dados da matriz.
+
+##### Saída:
+A saída contém a soma dos elementos acima da diagona principal.
+
+Exemplos:
+
+| Entrada:  | Saída:    |
+|-----------|-----------|
+| 3         | 12        |
+| 10 3 2    |           |
+| 5 15 7    |           |
+| 8 6 4     |           |
