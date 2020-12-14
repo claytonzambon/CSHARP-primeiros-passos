9a48cbc72c534faecef41aefbfcb90af46e4ed19
Clayton Zambon <zambon@outlook.com.br>
2020-12-14 11:51:42 -0300

    68.1-Exercicio de Matrizes

---
+++ 07-Matrizes/68.1_Exercicio-Matrizes/68.1_Exercicio-Matrizes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _68._1_Exercicio_Matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+            //Ler números inteiros para o tamanho da matriz
+            int M, N;
+
+            string[] s = Console.ReadLine().Split(' ');
+            M = int.Parse(s[0]);
+            N = int.Parse(s[1]);
+
+            //Declarar a Matriz de tamanho MxN
+            int[,] matriz = new int[M, N];
+
+            //Ler valores e populando a matriz
+            for (int i = 0; i < M; i++) {
+                s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    matriz[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            //Verificar e exibir valores NEGATIVOS
+            Console.WriteLine("VALORES NEGATIVOS:");
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (matriz[i, j] < 0) {
+                        Console.WriteLine(matriz[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 07-Matrizes/68.1_Exercicio-Matrizes/README.md
@@ -0,0 +1,17 @@
+# 68.1.Exercício - Matrizes
+##### Ler dois números M e N, e depois ler uma matriz MxN de números inteiros, conforme o exemplo. Em seguida, mostrar na tela somente os números negativos da matriz.
+
+##### Entrada:
+A entrada contém os números M e N na mesma linha, depois os dados da matriz.
+
+##### Saída:
+A saída contém uma mensagem e depois os números negativos da matriz, conforme exemplo.
+
+Exemplos:
+
+| Entrada:  | Saída:                |
+|-----------|-----------------------|
+| 2 3       | VALORES NEGATIVOS:    |
+| 12 -8 5   | -8                    |
+| -13 10 -6 | -13                   |
+|           | -6                    |
