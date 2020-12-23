958d6c7d5722933bdcb6a5d6803fbe7b10e66656
Clayton Zambon <zambon@outlook.com.br>
2020-12-23 15:04:20 -0300

    URI-1177 - Preenchimento de Vetor II

--- 06-Vetores/62.5._URI-1176/62.5._URI-1176/Program.cs
+++ 06-Vetores/62.5._URI-1176/62.5._URI-1176/Program.cs
@@ -12,11 +12,11 @@ namespace _62._5._URI_1176 {
             Int64[] vetFibo = new Int64[T];
 
             for (int i = 0; i < T; i++) {
-                int a = 0;
-                int b = 1;
+                Int64 a = 0;
+                Int64 b = 1;
                 int N = int.Parse(Console.ReadLine());
                 for (int j = 0; j < N; j++) {
-                    int aux = b;
+                    Int64 aux = b;
                     b = a;
                     a = aux + b;
                 }
---
+++ 06-Vetores/62.6._URI-1177/62.6._URI-1177/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _62._6._URI_1177 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI-1177
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1177
+             */
+
+            int T = int.Parse(Console.ReadLine());
+
+            int[] N = new int[1000];
+
+            for (int i = 0; i < 1000; i++) {
+                for (int j = 0; j < T; j++) {
+                    N[i] = j;
+                    i++;
+                    if (i == 1000) {
+                        break;
+                    }
+                }
+                i--;
+            }
+
+            //Exibir valores do vetor
+            for (int i = 0; i < 1000; i++) {
+                Console.WriteLine("N[" + i + "] = " + N[i]);
+            }
+        }
+    }
+}
