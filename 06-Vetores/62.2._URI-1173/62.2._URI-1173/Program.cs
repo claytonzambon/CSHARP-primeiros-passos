423d08ec43aa33340bfb7c987a01e4ec2266d0ff
Clayton Zambon <zambon@outlook.com.br>
2020-12-22 23:14:48 -0300

    URI-1173 - Preenchimento de Vetor I

---
+++ 06-Vetores/62.2._URI-1173/62.2._URI-1173/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _62._2._URI_1173 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI 1173
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1173
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            int[] vetor = new int[10];
+
+            for (int i = 0; i < 10; i++) {
+                vetor[i] = N;
+                N *= 2;
+            }
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine("N[" + i + "] = " + vetor[i]);
+            }
+        }
+    }
+}
