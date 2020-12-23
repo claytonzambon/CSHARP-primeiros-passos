8aa38d355dcb0c54efbe42c72fb8fe166797d172
Clayton Zambon <zambon@outlook.com.br>
2020-12-23 15:51:49 -0300

    URI-1178 - Preenchimento de Vetor III

---
+++ 06-Vetores/62.7._URI-1178/62.7._URI-1178/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace _62._7._URI_1178 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI-1178
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1178
+             */
+            
+            //Ler valor X
+            decimal X = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            //Declarar Vetor N
+            decimal[] N = new decimal[100];
+
+            //Preencher Vetor sempre com metade do valor anterior
+            for (int i = 0; i < 100; i++) {
+                N[i] = X;
+                X = X / 2;
+            }
+
+            //Exibir Vetor
+            for (int i = 0; i < 100; i++) {
+                Console.WriteLine("N[" + i + "] = " 
+                    + Math.Round(N[i], 4).ToString("F4", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
