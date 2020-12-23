547460fafc46ef705d7ac20fa77160ce3d05c4b3
Clayton Zambon <zambon@outlook.com.br>
2020-12-22 23:23:16 -0300

    URI-1174 - Selecao em Vetor I

---
+++ 06-Vetores/62.3._URI-1174/62.3._URI-1174/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _62._3._URI_1174 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI-1174
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1174
+             */
+
+            double[] A = new double[100];
+
+            for (int i = 0; i < 100; i++) {
+                A[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < 100; i++) {
+                if (A[i] <= 10) {
+                    Console.WriteLine("A[" + i + "] = " + A[i].ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
