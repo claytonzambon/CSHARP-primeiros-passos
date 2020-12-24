09fb9162c983849f32b99f40ff243cb31c9ae012
Clayton Zambon <zambon@outlook.com.br>
2020-12-23 23:22:15 -0300

    URI-1180 - Menor e Posicao

---
+++ 06-Vetores/62.9._URI-1180/62.9._URI-1180/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _62._9._URI_1180 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI - 1180
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1180
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            int[] X = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++) {
+                X[i] = int.Parse(s[i]);
+            }
+
+            int menor = X[0];
+            int posicao = 0;
+            for (int i = 0; i < N; i++) {
+                if (menor > X[i]) {
+                    menor = X[i];
+                    posicao = i;
+                }
+            }
+
+            Console.WriteLine("Menor valor: " + menor);
+            Console.WriteLine("Posicao: " + posicao);
+        }
+    }
+}
