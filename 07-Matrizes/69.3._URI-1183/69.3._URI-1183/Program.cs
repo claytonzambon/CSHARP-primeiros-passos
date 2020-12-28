fe1e7933bd6aebaff116cebf32200befca3fbb4a
Clayton Zambon <zambon@outlook.com.br>
2020-12-28 15:09:49 -0300

    URI-1183 - Acima da Diagonal Principal

---
+++ 07-Matrizes/69.3._URI-1183/69.3._URI-1183/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace _69._3._URI_1183 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio - URI - 1183
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1183
+             */
+
+            //Ler a operação desejada S ou M
+            char O = char.Parse(Console.ReadLine());
+
+            //Declarar a Matriz 12x12
+            double[,] M = new double[12, 12];
+
+            //Ler dados e popular a Matriz
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    M[i, j] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+            }
+
+            //Realizar a operação selecionada S ou M
+            // com os dados acima da Diagonal Principal
+            double soma = 0.0, resultado = 0.0;
+            int count = 0;
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    if (i < j && O == 'S') {
+                        resultado += M[i, j];
+                    }
+                    else if (i < j && O == 'M') {
+                        soma += M[i, j];
+                        count++;
+                        resultado = soma / count;
+                    }
+                }
+            }
+
+            //Exibir resultado
+            Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
