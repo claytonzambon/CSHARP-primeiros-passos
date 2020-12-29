9cdc01303241c2cd12229052d198dd84c1dcf9ec
Clayton Zambon <zambon@outlook.com.br>
2020-12-29 10:28:38 -0300

    URI-1190 - Area Direita

---
+++ 07-Matrizes/69.10._URI-1190/69.10._URI-1190/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace _69._10._URI_1190 {
+    class Program {
+        static void Main(string[] args) {
+            /* Exercicio URI-190
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1190
+             */
+
+            //Ler a operação desejada S ou M
+            char O = char.Parse(Console.ReadLine());
+
+            //Declarar a Matriz 12x12
+            double[,] M = new double[12, 12];
+
+            //Ler dados e popular a Matriz
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    M[i, j] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+            }
+
+            //Realizar a operação selecionada S ou M
+            //com os dados da Area Superior
+            double soma = 0.0, resultado = 0.0;
+            int count = 0;
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    //Se "i < j" e "i + j > 11" (tamanho máximo da matriz) 
+                    //realizamos a operação selecionada apenas com a Area Direita da Matriz
+                    if (i < j && (i + j) > 11) {
+                        if (O == 'S') {
+                            resultado += M[i, j];
+                        }
+                        else if (O == 'M') {
+                            soma += M[i, j];
+                            count++;
+                            resultado = soma / count;
+                        }
+                    }
+                }
+            }
+
+            //Exibir resultado
+            Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
