190b7b82079c82635610978d8b22c49ee2908706
Clayton Zambon <zambon@outlook.com.br>
2021-01-04 22:53:34 -0300

    URI-1759 - Ho Ho Ho

---
+++ 05-Estruturas_Repetitivas/52.36_URI-1759/52.36_URI-1759/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _52._36_URI_1759 {
+    class Program {
+        static void Main(string[] args) {
+            /* URI - 1759
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1759
+             */
+            
+            //Declarar variável e ler numero digitado
+            int N = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 0; i < N; i++) {
+                if (i < (N-1)) {
+                    Console.Write("Ho ");
+                }
+                else{
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
