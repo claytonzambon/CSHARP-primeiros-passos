926da9c01790c4e1253694257040b06a925990e8
Clayton Zambon <zambon@outlook.com.br>
2021-01-04 22:30:19 -0300

    URI-1564 - Vai Ter Copa?

---
+++ 05-Estruturas_Repetitivas/52.35_URI-1564/52.35_URI-1564/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _52._35_URI_1564 {
+    class Program {
+        static void Main(string[] args) {
+            /* URI - 1564
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1564
+             */
+
+            while (true) {
+
+                //Condição para satisfazer o EOF em C#
+                if (!int.TryParse(Console.ReadLine(), out int N)) {
+                    break;
+                }
+
+                if (N > 0) {
+                    Console.WriteLine("vai ter duas!");
+                }
+                else {
+                    Console.WriteLine("vai ter copa!");
+                }
+            }
+        }
+    }
+}
