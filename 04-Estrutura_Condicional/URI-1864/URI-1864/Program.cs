be56cde6cee0e3975e9d7fca8709336f1c3a7e3b
Clayton Zambon <zambon@outlook.com.br>
2021-01-11 16:27:39 -0300

    URI-1864 - Nossos Dias Nunca Voltarao

---
+++ 04-Estrutura_Condicional/URI-1864/URI-1864/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace URI_1864 {
+    class Program {
+        static void Main(string[] args) {
+            /* URI - 1864
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1864
+             */
+
+            int N = int.Parse(Console.ReadLine());
+
+            string frase = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+
+            for (int i = 0; i < N; i++) {
+                Console.Write(frase[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
