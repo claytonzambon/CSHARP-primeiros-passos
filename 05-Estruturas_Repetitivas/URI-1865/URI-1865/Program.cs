653d6dd236f9258e8f3f11e79f66ba46ebb1813f
Clayton Zambon <zambon@outlook.com.br>
2021-01-12 11:31:45 -0300

    URI - 1865 - Mjolnir

---
+++ 05-Estruturas_Repetitivas/URI-1865/URI-1865/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace URI_1865 {
+    class Program {
+        static void Main(string[] args) {
+            /* URI - 1865
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1865
+             */
+
+            //Declarar variável e ler valor digitado
+            int C = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < C; i++) {
+
+                string[] s = Console.ReadLine().Split(' ');
+
+                if (s[0].Equals("Thor")) {
+                    Console.WriteLine("Y");
+                }
+                else {
+                    Console.WriteLine("N");
+                }
+            }
+        }
+    }
+}
