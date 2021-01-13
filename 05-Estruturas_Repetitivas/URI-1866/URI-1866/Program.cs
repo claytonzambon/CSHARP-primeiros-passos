07d29e7f07ac683348373dfda98e8b77889bb277
Clayton Zambon <zambon@outlook.com.br>
2021-01-13 16:10:34 -0300

    URI-1866 - Conta

---
+++ 05-Estruturas_Repetitivas/URI-1866/URI-1866/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace URI_1866 {
+    class Program {
+        static void Main(string[] args) {
+            /* URI - 1866
+             * https://www.urionlinejudge.com.br/judge/pt/problems/view/1866
+             */
+
+            int C = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < C; i++) {
+                int S = int.Parse(Console.ReadLine());
+                int soma = 1;
+                for (int j = 0; j <= S; j++) {
+                    if (soma == 1) {
+                        soma -= 1;
+                    }
+                    else {
+                        soma += 1;
+                    }
+                }
+                Console.WriteLine(soma);
+            }
+        }
+    }
+}
